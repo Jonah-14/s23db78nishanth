39f5a556c61ed31579115879a07cabae26ca6710
Jonah-14 <142935188+Jonah-14@users.noreply.github.com>
2023-11-08 02:01:10 -0600

    updated icecream.js

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -22,15 +22,30 @@ exports.icecream_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-    
-// for a specific icecream.
+    // for a specific icecream.
 exports.icecream_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: icecream detail: ' + req.params.id);
 };
 // Handle icecream create on POST.
-exports.icecream_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: icecream create POST');
-};
+exports.icecream_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new icecream();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.flavor = req.body.flavor;
+    document.type = req.body.type;
+    document.rating = req.body.rating;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    }; 
 // Handle icecream delete form on DELETE.
 exports.icecream_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: icecream delete DELETE ' + req.params.id);
